e70651f35ee861372cdff0ff3fea877c931e99ab
George Raptis <georapbox@gmail.com>
2017-04-19 16:30:01 +0300

    Add button to erase search term

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,3 +1,4 @@
+import '../styles/search_bar.scss';
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
@@ -15,20 +16,31 @@ class SearchBar extends Component {
     this.props.onSearchTermChange(term);
   }
 
+  onEraseButtonClick() {
+    this.onInputChange('');
+  }
+
   render() {
     return (
       <div className="row">
         <div className="col-lg-6 offset-lg-3 mt-3 mb-3">
           <div className="search-bar">
-            <input
-              className="form-control"
-              type="text"
-              placeholder="Search..."
-              onChange={event => this.onInputChange(event.target.value)} />
+            <div className={this.state.term && 'input-group'}>
+              <input
+                className="form-control search-bar__input"
+                type="text"
+                placeholder="Search..."
+                autoFocus
+                value={this.state.term}
+                onChange={event => this.onInputChange(event.target.value)} />
+
+              <span
+                className={`input-group-addon close ${!this.state.term ? 'd-none' : ''}`}
+                onClick={() => this.onEraseButtonClick()}>&times;</span>
+            </div>
           </div>
         </div>
       </div>
-
     );
   }
 }
